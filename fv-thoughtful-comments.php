c3ea57e3c65c7da2463fea6b6350f60b9b7de0c2
Martin FV <martinv@foliovision.com>
2015-04-10 09:45:48 +0200

    broken: comment html caching works in progress

--- fv-thoughtful-comments.php
+++ fv-thoughtful-comments.php
@@ -161,7 +161,67 @@ class fv_tc extends fv_tc_Plugin {
         } 
         return $actions;
     }
+    
+    
+    
+    function cache_start( $args ) {
+      /*if( 1>0 ) {
+        return $args;
+      }*/
+      
+      global $wp_query, $post, $blog_id;
+      
+      $iComments = get_comments_number();
+      $bCommentAuthor = false;
+      $sCommentAuthor = false;
+      
+      foreach ($_COOKIE as $n => $v) {
+        if (substr($n, 0, 14) == 'comment_author') {
+          $bCommentAuthor = true;    
+        }
+      }
+      
+      $bCommenterUnapproved = false;
+      if( $wp_query->comments ) {
+        foreach( $wp_query->comments as $objComment ) {
+          if( $objComment->comment_author_email == $sCommentAuthor && $objComment->comment_approved == 0 ) {
+            $bCommenterUnapproved = true;
+          }
+        }
+      }
+      
+      
+      if( !$bCommenterUnapproved ) {
+        $bCommentAuthor = false;
+        echo "<!--fv comments cache - no unapproved comments for $sCommentAuthor - serving cached data -->\n";
+      } else {
+        echo "<!--fv comments cache - unapproved comments for $sCommentAuthor - not serving cached data -->\n";
+      }
+      
+      $cache = false;
+      $filename = $post->ID.'-'.$post->post_name.'.tmp';
+      $file = WP_CONTENT_DIR . '/fv-tc-cache-'.$blog_id.'/'.$filename;  //  check if exists!
 
+      if( file_exists( $file ) )
+        $cache = unserialize( file_get_contents( $file ) );
+      
+      if ( !is_array($cache) )
+        $cache = array();
+      
+      if( !is_user_logged_in() && !$bCommentAuthor && isset($cache['html']) && ($cache['date'] + 7200) > date( 'U' ) && isset($cache['comments']) && $cache['comments'] == $iComments && !isset( $_COOKIE['fv-debug'] ) ) {
+        echo $cache['html'];
+        echo "<!--fv comments cache from $filename @ ".$cache['date']."-->\n";
+        
+        //  skip the rest of the output!
+        $args['walker'] = new FV_TC_Walker_Comment_blank;
+      } else {echo "<!--fvtc FV_TC_Walker_Comment_capture -->\n";
+        $args['walker'] = new FV_TC_Walker_Comment_capture;        
+      }
+      
+      return $args;
+    }
+
+    
 
     /**
      * Filter for manage_users_columns to add new column into user management table
@@ -1280,6 +1340,14 @@ function fv_tc_x_add_metadata_field( $field_slug, $field, $object_type, $object_
   return $fv_tc->column_content( $field, $field_slug, $object_id );
 }
 
+
+//  todo: no better way?
+/*function fv_tc_comment_callback_blank( $comment, $args, $depth ) {
+  echo "<li style='display: none;'>";
+  return;
+}*/
+
+
 /* Add extra backend moderation options */
 add_filter( 'comment_row_actions', array( $fv_tc, 'admin' ) );
 
@@ -1346,7 +1414,6 @@ add_action('admin_init', array( $fv_tc, 'fv_tc_user_nicename_change' ) );
 /* Override Wordpress Blacklisting */
 //add_action( 'wp_blacklist_check', array( $fv_tc, 'blacklist' ), 10, 7 );
 
-endif;
 
 add_filter( 'comment_moderation_headers', array( $fv_tc, 'comment_moderation_headers' ) ); 
 
@@ -1368,3 +1435,419 @@ add_action('init', array($fv_tc, 'ap_action_init'));
 add_filter('get_comment_link', array($fv_tc, 'get_comment_link'));
 add_filter('get_comments_pagenum_link', array($fv_tc, 'get_comments_pagenum_link'));  //  todo: test!
 add_filter('paginate_links', array($fv_tc, 'get_comments_pagenum_link'));
+
+//  comments html caching
+add_filter( 'wp_list_comments_args', array($fv_tc, 'cache_start') );
+
+
+endif;
+
+
+
+class FV_TC_Walker_Comment_capture extends Walker_Comment {
+
+	/**
+	 * Start the list before the elements are added.
+	 *
+	 * @see Walker::start_lvl()
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int $depth Depth of comment.
+	 * @param array $args Uses 'style' argument for type of HTML list.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 1;
+
+		switch ( $args['style'] ) {
+			case 'div':
+				break;
+			case 'ol':
+				$output .= '<ol class="children">' . "\n";
+				break;
+			case 'ul':
+			default:
+				$output .= '<ul class="children">' . "\n";
+				break;
+		}
+    echo "<!--fvtc start 1-->\n";
+    if( $depth == 0 ) {echo "<!--fvtc start 2-->\n";
+      ob_start();
+    }
+	}
+
+	/**
+	 * End the list of items after the elements are added.
+	 *
+	 * @see Walker::end_lvl()
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth  Depth of comment.
+	 * @param array  $args   Will only append content if style argument value is 'ol' or 'ul'.
+	 */
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 1;
+
+		switch ( $args['style'] ) {
+			case 'div':
+				break;
+			case 'ol':
+				$output .= "</ol><!-- .children -->\n";
+				break;
+			case 'ul':
+			default:
+				$output .= "</ul><!-- .children -->\n";
+				break;
+		}
+    echo "<!--fvtc end 1-->\n";
+    if( $depth == 0 ) {echo "<!--fvtc end 2-->\n";
+      $cache['html'] = ob_get_clean();
+      $cache['date'] = date( 'U' );
+      $cache['comments'] = $iComments;
+      
+      echo $cache['html'];
+  
+      if( !is_user_logged_in() && !$bCommentAuthor && !isset( $_COOKIE['fv-debug'] ) ) {
+        echo "<!--fv comments cache into $filename @ ".$cache['date']."-->\n";
+        file_put_contents( $file, serialize( $cache ) );
+      } else {
+        echo "<!--fv comments do not cache into $filename @ ".$cache['date']." - user logged in or comment author-->\n";
+      }   
+    }    
+	}
+  
+}
+
+
+class FV_TC_Walker_Comment_blank extends Walker_Comment {
+
+	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * The $args parameter holds additional values that may be used with the child
+	 * class methods. This method is called at the start of the output list.
+	 *
+	 * @since 2.1.0
+	 * @abstract
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth  Depth of the item.
+	 * @param array  $args   An array of additional arguments.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {}
+
+	/**
+	 * Ends the list of after the elements are added.
+	 *
+	 * The $args parameter holds additional values that may be used with the child
+	 * class methods. This method finishes the list at the end of output of the elements.
+	 *
+	 * @since 2.1.0
+	 * @abstract
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth  Depth of the item.
+	 * @param array  $args   An array of additional arguments.
+	 */
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {}
+
+	/**
+	 * Start the element output.
+	 *
+	 * The $args parameter holds additional values that may be used with the child
+	 * class methods. Includes the element output also.
+	 *
+	 * @since 2.1.0
+	 * @abstract
+	 *
+	 * @param string $output            Passed by reference. Used to append additional content.
+	 * @param object $object            The data object.
+	 * @param int    $depth             Depth of the item.
+	 * @param array  $args              An array of additional arguments.
+	 * @param int    $current_object_id ID of the current item.
+	 */
+	public function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {}
+
+	/**
+	 * Ends the element output, if needed.
+	 *
+	 * The $args parameter holds additional values that may be used with the child class methods.
+	 *
+	 * @since 2.1.0
+	 * @abstract
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $object The data object.
+	 * @param int    $depth  Depth of the item.
+	 * @param array  $args   An array of additional arguments.
+	 */
+	public function end_el( &$output, $object, $depth = 0, $args = array() ) {}
+
+	/**
+	 * Traverse elements to create list from elements.
+	 *
+	 * Display one element if the element doesn't have any children otherwise,
+	 * display the element and its children. Will only traverse up to the max
+	 * depth and no ignore elements under that depth. It is possible to set the
+	 * max depth to include all depths, see walk() method.
+	 *
+	 * This method should not be called directly, use the walk() method instead.
+	 *
+	 * @since 2.5.0
+	 *
+	 * @param object $element           Data object.
+	 * @param array  $children_elements List of elements to continue traversing.
+	 * @param int    $max_depth         Max depth to traverse.
+	 * @param int    $depth             Depth of current element.
+	 * @param array  $args              An array of arguments.
+	 * @param string $output            Passed by reference. Used to append additional content.
+	 * @return null Null on failure with no changes to parameters.
+	 */
+	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+
+	}
+
+	/**
+	 * Display array of elements hierarchically.
+	 *
+	 * Does not assume any existing order of elements.
+	 *
+	 * $max_depth = -1 means flatly display every element.
+	 * $max_depth = 0 means display all levels.
+	 * $max_depth > 0 specifies the number of display levels.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param array $elements  An array of elements.
+	 * @param int   $max_depth The maximum hierarchical depth.
+	 * @return string The hierarchical item output.
+	 */
+	public function walk( $elements, $max_depth) {
+
+		$args = array_slice(func_get_args(), 2);
+		$output = '';
+
+		if ($max_depth < -1) //invalid parameter
+			return $output;
+
+		if (empty($elements)) //nothing to walk
+			return $output;
+
+		$parent_field = $this->db_fields['parent'];
+
+		// flat display
+		if ( -1 == $max_depth ) {
+			$empty_array = array();
+			foreach ( $elements as $e )
+				$this->display_element( $e, $empty_array, 1, 0, $args, $output );
+			return $output;
+		}
+
+		/*
+		 * Need to display in hierarchical order.
+		 * Separate elements into two buckets: top level and children elements.
+		 * Children_elements is two dimensional array, eg.
+		 * Children_elements[10][] contains all sub-elements whose parent is 10.
+		 */
+		$top_level_elements = array();
+		$children_elements  = array();
+		foreach ( $elements as $e) {
+			if ( 0 == $e->$parent_field )
+				$top_level_elements[] = $e;
+			else
+				$children_elements[ $e->$parent_field ][] = $e;
+		}
+
+		/*
+		 * When none of the elements is top level.
+		 * Assume the first one must be root of the sub elements.
+		 */
+		if ( empty($top_level_elements) ) {
+
+			$first = array_slice( $elements, 0, 1 );
+			$root = $first[0];
+
+			$top_level_elements = array();
+			$children_elements  = array();
+			foreach ( $elements as $e) {
+				if ( $root->$parent_field == $e->$parent_field )
+					$top_level_elements[] = $e;
+				else
+					$children_elements[ $e->$parent_field ][] = $e;
+			}
+		}
+
+		foreach ( $top_level_elements as $e )
+			$this->display_element( $e, $children_elements, $max_depth, 0, $args, $output );
+
+		/*
+		 * If we are displaying all levels, and remaining children_elements is not empty,
+		 * then we got orphans, which should be displayed regardless.
+		 */
+		if ( ( $max_depth == 0 ) && count( $children_elements ) > 0 ) {
+			$empty_array = array();
+			foreach ( $children_elements as $orphans )
+				foreach( $orphans as $op )
+					$this->display_element( $op, $empty_array, 1, 0, $args, $output );
+		 }
+
+		 return $output;
+	}
+
+	/**
+ 	 * paged_walk() - produce a page of nested elements
+ 	 *
+ 	 * Given an array of hierarchical elements, the maximum depth, a specific page number,
+ 	 * and number of elements per page, this function first determines all top level root elements
+ 	 * belonging to that page, then lists them and all of their children in hierarchical order.
+ 	 *
+	 * $max_depth = 0 means display all levels.
+	 * $max_depth > 0 specifies the number of display levels.
+	 *
+ 	 * @since 2.7.0
+	 *
+ 	 * @param int $max_depth The maximum hierarchical depth.
+ 	 * @param int $page_num  The specific page number, beginning with 1.
+ 	 * @return string XHTML of the specified page of elements
+ 	 */
+	public function paged_walk( $elements, $max_depth, $page_num, $per_page ) {
+
+		/* sanity check */
+		if ( empty($elements) || $max_depth < -1 )
+			return '';
+
+		$args = array_slice( func_get_args(), 4 );
+		$output = '';
+
+		$parent_field = $this->db_fields['parent'];
+
+		$count = -1;
+		if ( -1 == $max_depth )
+			$total_top = count( $elements );
+		if ( $page_num < 1 || $per_page < 0  ) {
+			// No paging
+			$paging = false;
+			$start = 0;
+			if ( -1 == $max_depth )
+				$end = $total_top;
+			$this->max_pages = 1;
+		} else {
+			$paging = true;
+			$start = ( (int)$page_num - 1 ) * (int)$per_page;
+			$end   = $start + $per_page;
+			if ( -1 == $max_depth )
+				$this->max_pages = ceil($total_top / $per_page);
+		}
+
+		// flat display
+		if ( -1 == $max_depth ) {
+			if ( !empty($args[0]['reverse_top_level']) ) {
+				$elements = array_reverse( $elements );
+				$oldstart = $start;
+				$start = $total_top - $end;
+				$end = $total_top - $oldstart;
+			}
+
+			$empty_array = array();
+			foreach ( $elements as $e ) {
+				$count++;
+				if ( $count < $start )
+					continue;
+				if ( $count >= $end )
+					break;
+				$this->display_element( $e, $empty_array, 1, 0, $args, $output );
+			}
+			return $output;
+		}
+
+		/*
+		 * Separate elements into two buckets: top level and children elements.
+		 * Children_elements is two dimensional array, e.g.
+		 * $children_elements[10][] contains all sub-elements whose parent is 10.
+		 */
+		$top_level_elements = array();
+		$children_elements  = array();
+		foreach ( $elements as $e) {
+			if ( 0 == $e->$parent_field )
+				$top_level_elements[] = $e;
+			else
+				$children_elements[ $e->$parent_field ][] = $e;
+		}
+
+		$total_top = count( $top_level_elements );
+		if ( $paging )
+			$this->max_pages = ceil($total_top / $per_page);
+		else
+			$end = $total_top;
+
+		if ( !empty($args[0]['reverse_top_level']) ) {
+			$top_level_elements = array_reverse( $top_level_elements );
+			$oldstart = $start;
+			$start = $total_top - $end;
+			$end = $total_top - $oldstart;
+		}
+		if ( !empty($args[0]['reverse_children']) ) {
+			foreach ( $children_elements as $parent => $children )
+				$children_elements[$parent] = array_reverse( $children );
+		}
+
+		foreach ( $top_level_elements as $e ) {
+			$count++;
+
+			// For the last page, need to unset earlier children in order to keep track of orphans.
+			if ( $end >= $total_top && $count < $start )
+					$this->unset_children( $e, $children_elements );
+
+			if ( $count < $start )
+				continue;
+
+			if ( $count >= $end )
+				break;
+
+			$this->display_element( $e, $children_elements, $max_depth, 0, $args, $output );
+		}
+
+		if ( $end >= $total_top && count( $children_elements ) > 0 ) {
+			$empty_array = array();
+			foreach ( $children_elements as $orphans )
+				foreach( $orphans as $op )
+					$this->display_element( $op, $empty_array, 1, 0, $args, $output );
+		}
+
+		return $output;
+	}
+
+	public function get_number_of_root_elements( $elements ){
+
+		$num = 0;
+		$parent_field = $this->db_fields['parent'];
+
+		foreach ( $elements as $e) {
+			if ( 0 == $e->$parent_field )
+				$num++;
+		}
+		return $num;
+	}
+
+	// Unset all the children for a given top level element.
+	public function unset_children( $e, &$children_elements ){
+
+		if ( !$e || !$children_elements )
+			return;
+
+		$id_field = $this->db_fields['id'];
+		$id = $e->$id_field;
+
+		if ( !empty($children_elements[$id]) && is_array($children_elements[$id]) )
+			foreach ( (array) $children_elements[$id] as $child )
+				$this->unset_children( $child, $children_elements );
+
+		if ( isset($children_elements[$id]) )
+			unset( $children_elements[$id] );
+
+	}
+
+} // Walker
