3fbf6bb05399b285296bf39283a2653800050184
Alec Kinnear <github@foliovision.com>
2016-10-17 21:06:06 +0200

    Update fv-thoughtful-comments.php

--- fv-thoughtful-comments.php
+++ fv-thoughtful-comments.php
@@ -3,7 +3,7 @@
 Plugin Name: FV Thoughtful Comments
 Plugin URI: http://foliovision.com/
 Description: Manage incomming comments more effectively by using frontend comment moderation system provided by this plugin.
-Version: 0.3.5.7
+Version: 0.3.5.9
 Author: Foliovision
 Author URI: http://foliovision.com/seo-tools/wordpress/plugins/thoughtful-comments/
 
@@ -63,7 +63,7 @@ class fv_tc extends fv_tc_Plugin {
    * Plugin version
    * @var string
    */
-  var $strVersion = '0.3.5.7';
+  var $strVersion = '0.3.5.9';
 
   /**
    * Decide if scripts will be loaded on current page
